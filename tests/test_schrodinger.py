606911304f72b74c75744b8b42f84481a1debe1d
Zane <hzeng3@u.rochester.edu>
2018-12-18 12:16:12 -0500

    Adding tests for functions

--- tests/test_schrodinger.py
+++ tests/test_schrodinger.py
@@ -8,7 +8,6 @@ import unittest
 import pytest
 import tensorflow as tf
 import schrodinger.schrodinger as schrodinger
-import math
 
 class TestSchrodinger(unittest.TestCase):
 	# def test_create_args(self):
@@ -16,7 +15,7 @@ class TestSchrodinger(unittest.TestCase):
 	# 	self.assertEqual(args.file, 'potential_energy.dat')
 	# 	self.assertEqual(args.c, 1.0)
 	# 	self.assertEqual(args.size, 5)
-		# self.assertEqual(args.domain, [0, 7])
+	# 	self.assertEqual(args.domain, [0, 7])
 
 
 	def test_open_file(self):
@@ -25,26 +24,15 @@ class TestSchrodinger(unittest.TestCase):
 		self.assertEqual(potential, [0.0, 6.0, 0.0, -6.0, 0.0, 6.0, 0.0])
 	
 	
-	def test_term(self):
-		function1 = schrodinger.term(0)
-		function2 = lambda x: tf.math.cos(0 * x)
-		self.assertTrue(tf.equal(function1(math.pi), function2(math.pi)))
+	# def test_term(self):
+	# 	function1 = schrodinger.term(0)
+	# 	function2 = lambda x: tf.math.cos(0 * x)
+	# 	self.assertTrue(tf.equal(function1(math.pi), function2(math.pi)))
 	# def test_create_basis(self):
 
 
-	def v0(position, potential, basis):
-    	_product = tf.constant([], dtype = tf.float32)
-    	for i in range(len(basis)):
-        	matrix1 = basis[i](position)
-        	matrix2 = tf.transpose(potential)
-        	product = matrix1 * matrix2
-        	product = tf.reduce_sum(product)
-        	product = tf.constant(product, shape = [1,])
-        	_product = tf.concat([_product, product], 0)
-		return _product
-	
 
-	def v0_hat(position, potential, basis):
+
 		
 
 if __name__ == '__main__':
